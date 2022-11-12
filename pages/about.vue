72175ddaa36e52ba3f8587ec78c9d015c481c3c5
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2022-11-12 16:12:40 +0800

    Revert "chore: add example"
    This reverts commit bf89c016d33802083e9c03b8581f78ae549017bb.

--- pages/about.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>My About Page</div>
-</template>
-
-<script>
-export default {}
-</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,7 @@
 <template>
-  <div>
-    <h1>{{ `${firstName} ${lastName}` }}</h1>
-
-    <v-btn> Primary </v-btn>
-  </div>
+  <div>Hello World</div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      firstName: 'John',
-      lastName: 'Doe',
-    }
-  },
-  // beforeCreate() {
-  //   console.log('beforeCreate is called')
-  // },
-  // beforeMount() {
-  //   console.log('beforeMounted is called')
-  // },
-  mounted() {
-    console.log('mounted is called')
-  },
-  beforeDestroy() {
-    console.log('beforeDestroy is called')
-    alert('Why leave me?')
-  },
-}
+export default {}
 </script>
-
-<style scoped>
-.error {
-  color: blue;
-}
-</style>
