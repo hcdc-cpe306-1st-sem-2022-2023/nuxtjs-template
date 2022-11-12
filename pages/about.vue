bf89c016d33802083e9c03b8581f78ae549017bb
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2022-11-12 16:11:03 +0800

    chore: add example

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>My About Page</div>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,37 @@
 <template>
-  <div>Hello World</div>
+  <div>
+    <h1>{{ `${firstName} ${lastName}` }}</h1>
+
+    <v-btn> Primary </v-btn>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Doe',
+    }
+  },
+  // beforeCreate() {
+  //   console.log('beforeCreate is called')
+  // },
+  // beforeMount() {
+  //   console.log('beforeMounted is called')
+  // },
+  mounted() {
+    console.log('mounted is called')
+  },
+  beforeDestroy() {
+    console.log('beforeDestroy is called')
+    alert('Why leave me?')
+  },
+}
 </script>
+
+<style scoped>
+.error {
+  color: blue;
+}
+</style>
